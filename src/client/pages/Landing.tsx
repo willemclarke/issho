99982da09fedbe3519dce148a13aabf691b7c604
Willem Clarke <willem.2@hotmail.com>
2020-09-05 00:49:52 +1000

    feat(landing): added button to randomly generate roomIds

--- src/client/pages/Landing.tsx
+++ src/client/pages/Landing.tsx
@@ -8,74 +8,77 @@ import { Messages } from '../../common/types';
 import { useAppContext } from '../hooks/useAppContext';
 import { useLocationQuery } from '../../client/hooks/useQuery';
 import { randomString } from '../../common/utils';
+import { useForm, Controller } from 'react-hook-form';
+
+interface FormValues {
+  username: string;
+  roomId: string;
+}
 
 export const Landing = () => {
   const { config } = useAppContext();
+  const { handleSubmit, control, setValue, setError, errors } = useForm<FormValues>();
+  const history = useHistory();
+  const roomName = useLocationQuery().get('roomName');
 
   const [socket] = useSocket(config.webSocketApi, {
     autoConnect: true,
     secure: config.webSocketSecure,
   });
-  const history = useHistory();
-
-  const roomName = useLocationQuery().get('roomName');
-  const [username, setUsername] = React.useState<string>('');
-  const [roomId, setRoomId] = React.useState(roomName);
-  const [error, setError] = React.useState<string | null>(null);
-  const [randomlyGeneratedRoomName, setRandomlyGeneratedRoomName] = React.useState<
-    string | undefined
-  >(undefined);
-  console.log('randomly generated roomName: ', randomlyGeneratedRoomName);
 
   React.useEffect(() => {
     socket.on(Messages.CAN_JOIN_ROOM_RESPONSE, (data: any) => {
       if (data.error) {
-        setError(data.error);
+        setError('username', { message: data.error });
       } else {
         history.push(`/rooms/${data.roomId}?username=${data.username}`);
       }
     });
   }, [history, socket]);
 
-  const onSubmit = () => {
-    socket.emit(Messages.CAN_JOIN_ROOM_REQUEST, { roomId, username });
+  const onSubmit = (values: FormValues) => {
+    const { roomId, username } = values;
+    socket.emit(Messages.CAN_JOIN_ROOM_REQUEST, { roomId: roomName || roomId, username });
   };
 
   const onClick = () => {
     const generatedRoomName = _.toUpper(randomString(6));
-    setRandomlyGeneratedRoomName(generatedRoomName);
+    setValue('roomId', generatedRoomName);
   };
 
-  const duplicateUserError = error ? <Alert message={error} type="error" banner /> : null;
+  const UserErrors = () => (
+    <>
+      {_.map(errors, (error, key) => (
+        <Alert message={error?.message} type="error" banner />
+      ))}
+    </>
+  );
 
   return (
-    <Row justify="center" style={{ marginTop: '2rem' }}>
-      <Col xs={4} xxl={5}>
-        <Form name="landing" className="landing-form" onFinish={onSubmit}>
-          {_.isNil(roomName) ? (
-            <Form.Item
-              name="roomId"
-              rules={[{ required: true, message: 'Please enter room name' }]}
-            >
-              <Button onClick={onClick} icon={<HighlightOutlined />}></Button>;
-              <Input
-                type="roomId"
-                placeholder="Enter room name"
-                onChange={(e) => setRoomId(e.target.value)}
-                value={_.isNil(randomlyGeneratedRoomName) ? roomId : randomlyGeneratedRoomName}
+    <>
+      <UserErrors />
+      <Row justify="center" style={{ marginTop: '2rem' }}>
+        <Col xs={4} xxl={5}>
+          <form name="landing" className="landing-form" onSubmit={handleSubmit(onSubmit)}>
+            {_.isNil(roomName) ? (
+              <Controller
+                control={control}
+                name={'roomId'}
+                rules={{ required: 'Please enter room name' }}
+                as={
+                  <Input
+                    placeholder="Enter room name"
+                    addonAfter={<HighlightOutlined onClick={onClick} />}
+                  />
+                }
               />
-            </Form.Item>
-          ) : null}
-          <Form.Item name="username" rules={[{ required: true, message: 'Please enter username' }]}>
-            <Input
-              placeholder="Enter username"
-              onChange={(e) => {
-                setUsername(e.target.value);
-              }}
+            ) : null}
+            <Controller
+              control={control}
+              name={'username'}
+              rules={{ required: 'Please enter username' }}
+              as={<Input placeholder="Enter username" />}
             />
-          </Form.Item>
-          {duplicateUserError}
-          <Form.Item>
             <Button
               type="primary"
               htmlType="submit"
@@ -84,9 +87,9 @@ export const Landing = () => {
             >
               Join Room
             </Button>
-          </Form.Item>
-        </Form>
-      </Col>
-    </Row>
+          </form>
+        </Col>
+      </Row>
+    </>
   );
 };
